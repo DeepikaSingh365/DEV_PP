4e161a303201f3f0ee76869b1eaa79ca8d970b30
Deepika <deepika22510@gmail.com>
2021-03-06 22:03:19 +0530

    folderSort

--- Lec1/function.js
+++ Lec1/function.js
@@ -1,26 +1,26 @@
   
-// High Order Functions => Functions which accept functions as an argument !
-// Callback Functions => Functions which are passed into a function as an argument !
+// // High Order Functions => Functions which accept functions as an argument !
+// // Callback Functions => Functions which are passed into a function as an argument !
 
-function getFirstName(fullName){
-    // fullName = "TONY STARK".split(" ") => [ "TONY" , "STARK"  ];
-    fullName = fullName.split(" ");
-    return fullName[0];
-}
+// function getFirstName(fullName){
+//     // fullName = "TONY STARK".split(" ") => [ "TONY" , "STARK"  ];
+//     fullName = fullName.split(" ");
+//     return fullName[0];
+// }
 
-function getLastName(fullName){
-    fullName = fullName.split(" ");
-    return fullName[1];
-}
+// function getLastName(fullName){
+//     fullName = fullName.split(" ");
+//     return fullName[1];
+// }
 
-function sayHi( fullName  , fun  ){
-    let name = fun(fullName);
-    console.log(name);
-}
+// function sayHi( fullName  , fun  ){
+//     let name = fun(fullName);
+//     console.log(name);
+// }
 
 
-sayHi("TONY STARK" , getFirstName);
-sayHi("BRUCE BANNER" ,  getLastName);
+// sayHi("TONY STARK" , getFirstName);
+// sayHi("BRUCE BANNER" ,  getLastName);
 // function body // function statement
 
 // let sayHi = function(){
@@ -40,7 +40,7 @@ function sayHi(name){
 
 // let value = sayHi();
 // console.log( value );
-console.log(sayHi);
+console.log(sayHi("deepika"));
 
 let value = sayHi("Steve");
 console.log(value);
